251ebe7d2aca0cf408f2bee2865ddcf5f62134d7
Dhanusha1 <87644268+Dhanusha1@users.noreply.github.com>
2021-07-19 16:09:36 +0530

    Add files via upload

---
+++ AreaRectangle.java
@@ -0,0 +1,13 @@
+package com.ust;
+
+public class AreaRectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double length=10.0,breadth=5.0,area;
+		area = length * breadth;
+		System.out.println("The area of the rectangle is : "+area);
+
+	}
+
+}
